0b1438256074f1018607dcf21b565dfcc2dbb224
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-25 23:17:12 +0000

    updated

--- 1_queries/05_selection.sql
+++ 1_queries/05_selection.sql
@@ -8,3 +8,13 @@ and users.id = 1
 group by properties.id, reservations.id
 order by reservations.start_date;
 
+-- select properties.id, properties.title, properties.cost_per_night, avg(property_reviews.rating) as average_rating
+-- from properties
+-- join reservations on reservations.property_id = properties.id
+-- join users on users.id = reservations.guest_id
+-- join property_reviews on property_reviews.property_id = properties.id
+-- where reservations.end_date < now()::date
+-- and users.id = 1
+-- group by properties.id;
+
+
